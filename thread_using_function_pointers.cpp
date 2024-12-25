39d0758dd5b21ee6ab4f118fe545fd33d3e37ab9
anil1791 <1234anil2@gmail.com>
2024-12-25 16:12:13 +0530

    Update thread_using_function_pointers.cpp

--- thread_using_function_pointers.cpp
+++ thread_using_function_pointers.cpp
@@ -12,9 +12,54 @@ void findSum(int start, int end)
     }
     cout << sum << endl;
 }
+class Base{
+    public:
+    void print(int x){
+        while(x>0){
+            cout<<x<<" ";
+            x--;
+        }
+    }
+};
+
+class Car{
+    public:
+    static void drive(int speed){
+        int x = 0;  cout<<"Accelerating from to "<<speed<<endl;
+        while(x<speed){
+            cout<<x<<" ";
+            x++;
+        }
+    }
+};
 
 int main()
 {
+    //THREAD using static member function
+
+    std::thread t1(&Car::drive,40);
+    if(t1.joinable())
+        t1.join();
+
+
+    /*
+    //THREAD using non-static member function
+
+    Base b;
+    std::thread t1(&Base::print,&b,5);
+    t1.join();
+    */
+    /*
+    // THREAD using LAMBDA function
+    auto function = [](int x)
+    {
+        while (x > 0)
+        {
+            cout << x << " ";
+            x--;
+        }
+    };*/
+    /*
     auto startTime = high_resolution_clock::now();
     std::thread t1(findSum, 1, 100909099);
     std::thread t2(findSum, 1, 100909099);
@@ -25,4 +70,5 @@ int main()
     auto endTime = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(endTime - startTime);
     cout << "Time in microseconds :- " << (duration.count()) << endl;
+    */
 }
